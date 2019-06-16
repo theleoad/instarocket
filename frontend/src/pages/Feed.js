328c79ce588bd434375d287889f769be0611c5ea
Leo <talkto@theleoad.com>
2019-06-16 16:36:01 -0300

    finish web intarface

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import api from '../services/api';
+import io from 'socket.io-client';
 
 import './Feed.css';
 
@@ -14,16 +15,38 @@ class Feed extends Component {
     };
     
     async componentDidMount() {
+        this.resgisterToSocket();
+
         const response = await api.get('posts');
 
         this.setState({ feed: response.data });
+    }
+
+    resgisterToSocket = () => {
+        const socket = io('http://localhost:3333');
+
+        socket.on('post', newPost => {
+            this.setState({ feed: [newPost, ...this.state.feed] });
+        })
+
+        socket.on('like', likedPost => {
+            this.setState( {
+                feed: this.state.feed.map(post =>
+                    post._id === likedPost._id ? likedPost : post
+                )
+            })
+        })
+    }
+
+    handleLike = id => {
+        api.post(`/posts/${id}/like`);
     }
 
     render() {
         return (
             <section id="post-list">
                 { this.state.feed.map(post => (
-                    <article>
+                    <article key={post._id}>
                     <header>
                         <div className="user-info">
                             <span>{post.author}</span>
@@ -34,7 +57,9 @@ class Feed extends Component {
                     <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
                     <footer>
                         <div className="actions">
-                            <img src={like} alt=""/>
+                            <button type="button" onClick={() => this.handleLike(post._id)}>
+                                <img src={like} alt=""/>
+                            </button>
                             <img src={comment} alt=""/>
                             <img src={send} alt=""/>
                        </div>
